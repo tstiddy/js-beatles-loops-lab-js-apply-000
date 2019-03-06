372dfea84c13f685f0ca170de12e2ae4035ab75a
Toby Stimpson <tobystimpson@mail.com>
2019-03-06 16:27:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]; //declare arrays
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]; //declare arrays
 
-var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
   var array = []; //create array 
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
   return array; //returns the result
 }
 
-var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 
 function johnLennonFacts(facts) {
   var array = [];
