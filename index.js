e9c8b0118981172794c33603b20a1529fc2c246b
Toby Stimpson <tobystimpson@mail.com>
2019-03-06 16:08:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ function theBeatlesPlay(musicians, instruments) {
 var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 
 function johnLennonFacts(facts) {
-  while
+  while () {
+    
+  }
 }
 
