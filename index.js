b94d8f75dfd6d49c90ee5001ae5dc81b79b2c33b
Toby Stimpson <tobystimpson@mail.com>
2019-03-06 15:59:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var musicians = ["John", "Paul", "George", "Ringo"];
+var musicians = ["John", "Paul", "George", "Ringo"]; //declare arrays
 
 var instruments = ["Guitar", "Guitar", "Bass", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (let i=0; i<musicians.length; i++) {
+  var array = []; //create array 
+  for (let i=0; i<musicians.length; i++) { 
     array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return array;
