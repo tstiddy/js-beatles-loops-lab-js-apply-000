6f2009c9402ea8a52129f622d6b5cabbc44df55e
Toby Stimpson <tobystimpson@mail.com>
2019-03-06 15:42:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var instruments = ["Guitar", "Guitar", "Bass", "Drums"];
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (let i = 0; i < 4; i++) {
-    array.push
+    array.push('')
   }
 }
