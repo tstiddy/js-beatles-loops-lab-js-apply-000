6d33a4fbc342c4f5ef285f54b375ee070b1df76d
Toby Stimpson <tobystimpson@mail.com>
2019-03-06 16:08:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
   var array = []; //create array 
-  for (let i=0; i<musicians.length; i++) { 
-    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  for (let i=0; i<musicians.length; i++) { //creates counter that counts how many musicians there are
+    array.push(`${musicians[i]} plays ${instruments[i]}`); //convines the musicians and instruments together
   }
-  return array;
+  return array; //returns the result
 }
 
 var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+
+function johnLennonFacts()
 
